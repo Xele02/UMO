b5754811a63c7bfb66accfdd756a98be67d3e66c
Xele02 <git@xele.org>
2022-11-08 11:17:58 +0100

    Implement UIPriority

--- Unity/Assets/UMAssets/Scripts/XeApp/Game/RhythmGame/UI/UIPriority.cs
+++ Unity/Assets/UMAssets/Scripts/XeApp/Game/RhythmGame/UI/UIPriority.cs
@@ -3,14 +3,16 @@ using System;
 
 namespace XeApp.Game.RhythmGame.UI
 {
+	[ExecuteInEditMode] // RVA: 0x650F48 Offset: 0x650F48 VA: 0x650F48
 	public class UIPriority : MonoBehaviour
 	{
 		[Serializable]
 		public struct PrioritySet
 		{
-			public Renderer renderer;
-			public int offset;
-			public UIPriority.Priority priority;
+			public Renderer renderer; // 0x0
+			[RangeAttribute(-200, 200)] // RVA: 0x68F0F4 Offset: 0x68F0F4 VA: 0x68F0F4
+			public int offset; // 0x4
+			public Priority priority; // 0x8
 		}
 
 		public enum Priority
@@ -26,10 +28,53 @@ namespace XeApp.Game.RhythmGame.UI
 		}
 
 		[SerializeField]
-		public PrioritySet[] m_meshPrioritySets;
+		public PrioritySet[] m_meshPrioritySets; // 0xC
 		[SerializeField]
-		public PrioritySet[] m_particlePrioritySets;
+		public PrioritySet[] m_particlePrioritySets; // 0x10
 		[SerializeField]
-		public PrioritySet[] m_linePrioritySets;
+		public PrioritySet[] m_linePrioritySets; // 0x14
+		private readonly float[] m_zPositionTable = new float[6] { 0, 0, 0, 0, 0, 0/*D8FE3210563748CD705E471FA2B460064F7B9843 */}; // 0x18
+		private const float m_sortingOrderRange = 200;
+
+		// RVA: 0x1567A14 Offset: 0x1567A14 VA: 0x1567A14
+		private void Start()
+		{
+			UnityEngine.Debug.LogError("Fix m_zPositionTable array");
+			for(int i = 0; i < m_meshPrioritySets.Length; i++)
+			{
+				if (m_meshPrioritySets[i].renderer != null)
+					Apply(ref m_meshPrioritySets[i]);
+			}
+			for(int i = 0; i < m_particlePrioritySets.Length; i++)
+			{
+				if(m_particlePrioritySets[i].renderer != null)
+					Apply(ref m_particlePrioritySets[i]);
+			}
+			for(int i = 0; i < m_linePrioritySets.Length; i++)
+			{
+				if (m_linePrioritySets[i].renderer != null)
+					Apply(ref m_linePrioritySets[i]);
+			}
+		}
+
+		//// RVA: 0x1567A18 Offset: 0x1567A18 VA: 0x1567A18
+		//private int CalcSortingOrder(UIPriority.Priority priority, float offset) { }
+
+		//// RVA: 0x15593BC Offset: 0x15593BC VA: 0x15593BC
+		//public void Apply() { }
+
+		//// RVA: 0x1567A74 Offset: 0x1567A74 VA: 0x1567A74
+		private void Apply(ref UIPriority.PrioritySet set)
+		{
+			if (set.priority == Priority.SystemUI)
+				set.renderer.sortingLayerName = "SystemUI";
+			else if (set.priority == Priority.Default)
+				set.renderer.sortingLayerName = "Default";
+			else if (set.priority == Priority.TopMost)
+				set.renderer.sortingLayerName = "Forward";
+			else
+				set.renderer.sortingLayerName = "Back";
+			set.renderer.sortingOrder = set.priority == Priority.Default ? Mathf.RoundToInt((set.offset + 100) * 200.0f / 200.0f) : Mathf.RoundToInt((set.offset + 100) * 200.0f / 200.0f - (int)set.priority * 200.0f);
+		}
 	}
 }
