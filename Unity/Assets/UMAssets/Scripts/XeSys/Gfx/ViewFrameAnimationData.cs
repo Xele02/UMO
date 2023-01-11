23d358fd6b628b6eb11f84d515b9650083e4dde2
Xele02 <git@xele.org>
2023-01-11 02:04:03 +0100

    Fix Scale/Rotation animation

--- Unity/Assets/UMAssets/Scripts/XeSys/Gfx/ViewFrameAnimationData.cs
+++ Unity/Assets/UMAssets/Scripts/XeSys/Gfx/ViewFrameAnimationData.cs
@@ -21,8 +21,8 @@ namespace XeSys.Gfx
 		private int m_FrameNum; // 0x1C
 		private Dictionary<string, int> m_LabelIndex = new Dictionary<string, int>(); // 0x20
 
-		// public FrameData[] F { get; } 0x1EEBECC
-		// public int FrameDataCount { get; } 0x1EEBCE4
+		public FrameData[] F { get { return m_FrameDataList; } } //0x1EEBECC
+		public int FrameDataCount { get { return m_FrameDataList != null ? m_FrameDataList.Length : 0; } } //0x1EEBCE4
 		// public bool IsPosAnim { get; set; } 0x1EEBED4 0x1EEBEE0
 		// public bool IsMoveAnim { get; set; } 0x1EEBEF8 0x1EEBF04
 		// public bool IsMoveBezierX { get; set; } 0x1EEBF1C 0x1EEBF28
@@ -270,6 +270,11 @@ namespace XeSys.Gfx
 				isUpdateSRT = true;
 			}
 			if((m_AnimFlag & 32) != 0)
+			{
+				vt.m.Scale = m_FrameDataList[Base].TransFormData.m.Scale + (m_FrameDataList[Target].TransFormData.m.Scale - m_FrameDataList[Base].TransFormData.m.Scale) * rate;
+				isUpdateSRT = true;
+			}
+			if((m_AnimFlag & 64) != 0)
 			{
 				vt.m.Rot = m_FrameDataList[Base].TransFormData.m.Rot + (m_FrameDataList[Target].TransFormData.m.Rot - m_FrameDataList[Base].TransFormData.m.Rot) * rate;
 				isUpdateSRT = true;
