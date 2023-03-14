eded2d6a06be1fe5926a44febb5c7e5068ae07ce
Xele02 <git@xele.org>
2023-03-14 11:03:48 +0100

    Todo abstract

--- Unity/Assets/UMAssets/Scripts/Stubs/Assembly-CSharp/XeApp/Game/Adv/AdvFadeMessage.cs
+++ Unity/Assets/UMAssets/Scripts/Stubs/Assembly-CSharp/XeApp/Game/Adv/AdvFadeMessage.cs
@@ -2,9 +2,25 @@ namespace XeApp.Game.Adv
 {
 	public class AdvFadeMessage : AdvMessageBase
 	{
+		public override bool IsPlay()
+		{
+			TodoLogger.Log(0, "Implement monobehaviour");
+			return false;
+		}
+
+		public override void Skip()
+		{
+			TodoLogger.Log(0, "Implement monobehaviour");
+		}
+
+		public override void StartMessage(string message, float message_spd, TagConvertFunc func)
+		{
+			TodoLogger.Log(0, "Implement monobehaviour");
+		}
+
 		private void Awake()
 		{
-			UnityEngine.Debug.LogError("Implement monobehaviour");
+			TodoLogger.Log(0, "Implement monobehaviour");
 		}
 	}
 }
