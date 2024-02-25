ec043c733c4b2e598f7b804fb27001b63be3493e
Xele02 <git@xele.org>
2024-02-25 15:57:17 +0100

    Move implemented files out of stubs dir

--- Unity/Assets/UMAssets/Scripts/Stubs/Assembly-CSharp/XeSys/Gfx/ImageEx.cs
+++ Unity/Assets/UMAssets/Scripts/Stubs/Assembly-CSharp/XeSys/Gfx/ImageEx.cs
@@ -14,6 +14,11 @@ namespace XeSys.Gfx
 		[SerializeField]
 		private string m_TextureName;
 
+		private void Awake()
+		{
+			TodoLogger.LogError(0, "Implement Monobehaviour");
+		}
+		
 		public Material MaterialMul { get => null; set { } } //throw new System.NotImplementedException(); set => null; //throw new System.NotImplementedException(); }
 		public Material MaterialAdd { get => null; set { } } //throw new System.NotImplementedException(); set => null; //throw new System.NotImplementedException(); }
 		}
